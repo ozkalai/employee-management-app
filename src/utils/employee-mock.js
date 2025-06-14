a524bc151dc5fbad66ae25a7f0dc6f82ddc51928
ozkalai <melihozkalayy@gmail.com>
2025-06-14 18:24:50 +0300

    Add mock employee generator

---
+++ src/utils/employee-mock.js
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker';
+
+const DEPARTMENTS = ['Analytics', 'Tech'];
+const POSITIONS = ['Junior', 'Medior', 'Senior'];
+
+export function generateEmployee() {
+  return {
+    id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    dateOfEmployment: faker.date.past(10).toISOString().split('T')[0],
+    dateOfBirth: faker.date.birthdate({ min: 1960, max: 2002, mode: 'year' }).toISOString().split('T')[0],
+    phoneNumber: faker.phone.number('+90 5## ### ## ##'),
+    email: faker.internet.email(),
+    department: faker.helpers.arrayElement(DEPARTMENTS),
+    position: faker.helpers.arrayElement(POSITIONS),
+  };
+}
+
+export function generateEmployees(count = 10) {
+  return Array.from({ length: count }, () => generateEmployee() );
+}
+
+export { DEPARTMENTS, POSITIONS }; 
